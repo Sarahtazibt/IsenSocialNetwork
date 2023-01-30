8dba25f679f1571a0c452464a62936f9920df077
Sarah Tazibt <sarahtazib17@gmail.com>
2023-01-30 12:08:06 +0100

    creation

---
+++ app/src/main/java/fr/isen/tazibt/isensocialnetwork/MainActivity.kt
@@ -0,0 +1,15 @@
+package fr.isen.tazibt.isensocialnetwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Write a message to the database
+        Firebase.database.getReference("message").setValue("Hello, World!")
+    }
+}
