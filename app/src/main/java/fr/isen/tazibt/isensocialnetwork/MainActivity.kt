c5c60e2b72df63ecf406353757735e874f0dcd9e
user name <user email>
2023-02-03 14:03:21 +0100

    la liste des posts ne marche pas

--- app/src/main/java/fr/isen/tazibt/isensocialnetwork/MainActivity.kt
+++ app/src/main/java/fr/isen/tazibt/isensocialnetwork/MainActivity.kt
@@ -2,21 +2,28 @@ package fr.isen.tazibt.isensocialnetwork
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.getValue
+import fr.isen.tazibt.isensocialnetwork.databinding.ActivityCreationBinding
+import fr.isen.tazibt.isensocialnetwork.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var mDataBase: DatabaseReference
-    private lateinit var postList:ArrayList<Post>
-    private lateinit var mAdapter:PostAdapter
+    private lateinit var postList: ArrayList<Post>
+    private lateinit var mAdapter: PostAdapter
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         /**initialized*/
         postList = ArrayList()
         mAdapter = PostAdapter(postList) {
@@ -24,20 +31,20 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("post", it)
             startActivity(intent)
         }
-        recyclerPosts.layoutManager = LinearLayoutManager(this)
-        recyclerPosts.setHasFixedSize(true)
-        // recyclerPosts.adapter = mAdapter
+        binding.recyclerPost.layoutManager = LinearLayoutManager(this)
+        binding.recyclerPost.adapter = mAdapter
         /**getData firebase*/
         getPosts()
 
     }
+
     /**ok now create new activity*/
 
 
     private fun getPosts() {
 
         mDataBase = FirebaseDatabase.getInstance().getReference("posts")
-        mDataBase.addValueEventListener(object :ValueEventListener{
+        mDataBase.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val posts = snapshot.children.map {
                     it.getValue<Post>()
@@ -46,16 +53,14 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(this@MainActivity,
-                    error.message, Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@MainActivity,
+                    error.message, Toast.LENGTH_SHORT
+                ).show()
             }
 
         })
 
 
     }
-
-
-
-
 }
--- app/src/main/java/fr/isen/tazibt/isensocialnetwork/PostAdapter.kt
+++ app/src/main/java/fr/isen/tazibt/isensocialnetwork/PostAdapter.kt
@@ -1,32 +1,43 @@
 package fr.isen.tazibt.isensocialnetwork
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import fr.isen.tazibt.isensocialnetwork.databinding.ActivityPostAdapterBinding
 
 class PostAdapter(
     var postList:ArrayList<Post>, var onClickListener: (Post) -> Unit
 ):RecyclerView.Adapter<PostAdapter.PostViewHolder>()
 {
-    inner class PostViewHolder(var v:ActivityPostAdapterBinding): RecyclerView.ViewHolder(v.root){}
+    class PostViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val postImg = view.findViewById<ImageView>(R.id.postImg)
+        val postBrand = view.findViewById<TextView>(R.id.postBrand)
+        val postColor = view.findViewById<TextView>(R.id.postColor)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
-        val inflter = LayoutInflater.from(parent.context)
-        val v = DataBindingUtil.inflate<ActivityPostAdapterBinding>(
-            inflter, R.layout.activity_post_adapter,parent,
-            false)
-        return PostViewHolder(v)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.activity_post_adapter, parent, false)
+        return PostViewHolder(view)
 
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
         val post = postList[position]
-        holder.v.isPosts = post
-        holder.v.root.setOnClickListener {
-            onClickListener(post)
+        holder.postBrand.text = post.carBrand
+        holder.postColor.text = post.carColor
+
+        val firstImage = post.carImg[0]
+        if (firstImage.isNotEmpty()){
+            Picasso.get().load(firstImage).into(holder.postImg)
         }
+        holder.postBrand.text
+
     }
 
     override fun getItemCount(): Int {
